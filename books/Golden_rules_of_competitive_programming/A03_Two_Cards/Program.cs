c37301351c36a8f5056c7f17a27060b1e2550330
itotto <itotto0205@gmail.com>
2024-04-07 16:37:16 +0900

    A03 - Two Cards追加 A04 - Binary Representation 1追加

---
+++ books/Golden_rules_of_competitive_programming/A03_Two_Cards/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace A03_Two_Cards {
+    internal class Program {
+        /// <summary>
+        /// A03 - Two Cards
+        /// </summary>
+        /// <remarks>https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_c</remarks>
+        static void Main() {
+            var conditions = Console.ReadLine()?.Split(' ');
+            if (conditions == null) return;
+            var n = Convert.ToInt32(conditions[0]);
+            var k = Convert.ToInt32(conditions[1]);
+
+            var redCards = new Dictionary<int, bool>(n);
+
+            // 赤いカードの情報を取得
+            var r_inputs = Console.ReadLine()?.Split(' ');
+            if (r_inputs == null) return;
+            foreach (var red in r_inputs) {
+                // kにするために必要な値に変換する
+                var kred = k - Convert.ToInt32(red);
+                if (!redCards.ContainsKey(kred)) {
+                    redCards.Add(kred, true);
+                }
+            }
+
+            var r = false;
+
+            // 青いカードの情報を取得
+            var b_inputs = Console.ReadLine()?.Split(' ');
+            if (b_inputs == null) return;
+            foreach (var blue in b_inputs) {
+                var b_value = Convert.ToInt32(blue);
+                if (redCards.ContainsKey(b_value)) {
+                    r = true;
+                    break;
+                }
+            }
+            Console.WriteLine(r ? "Yes" : "No");
+        }
+    }
+}
---
+++ books/Golden_rules_of_competitive_programming/A04_BinaryRepresentation1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace A04_BinaryRepresentation1 {
+    internal class Program {
+        /// <summary>
+        /// A04 - Binary Representation 1
+        /// </summary>
+        /// <remarks>https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_d</remarks>
+        static void Main() {
+            var nthPower = new List<int> {512, 256, 128, 64, 32, 16, 8, 4, 2, 1 };
+            var n = Convert.ToInt32(Console.ReadLine());
+
+            var r = new StringBuilder();
+            foreach (var p in nthPower) {
+                if (n >= p) {
+                    r.Append("1");
+                    n -= p;
+                } else {
+                    r.Append("0");
+                }
+            }
+            Console.WriteLine(r.ToString());
+        }
+    }
+}
