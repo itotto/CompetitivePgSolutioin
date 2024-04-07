c87d871c8f999dda2f8ab599babba08674025a18
itotto <itotto0205@gmail.com>
2024-04-07 15:43:19 +0900

    A01 - The First Problem追加 A02 - Linear Search追加

---
+++ books/Golden_rules_of_competitive_programming/A01_The_First_Problem/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace A01_The_First_Problem {
+    internal class Program {
+        /// <summary>
+        /// A01 The First Problem
+        /// </summary>
+        /// <remarks>https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_a</remarks>
+        static void Main() {
+            var n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(n * n);
+        }
+    }
+}
---
+++ books/Golden_rules_of_competitive_programming/A02_Linear_Search/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace A02_Linear_Search {
+    internal class Program {
+        /// <summary>
+        /// A02 - Linear Search
+        /// </summary>
+        /// <remarks>https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_b</remarks>
+        static void Main() {
+            var c = Console.ReadLine()?.Split(' ');
+            if (c == null) return;
+
+            var data = Console.ReadLine()?.Split(' ');
+            if (data == null) return;
+            var r = false;
+            foreach (var d in data) {
+                if (d == c[1]) {
+                    r = true;
+                    break;
+                }
+            }
+            Console.WriteLine(r ? "Yes" : "No");
+        }
+    }
+}
