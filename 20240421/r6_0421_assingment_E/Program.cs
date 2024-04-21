11bc901cf45bf15f2b2865b1d68635b8e0d54644
itotto0205 <itotto0205@gmail.com>
2024-04-21 23:17:29 +0900

    Delete 20240421 directory

--- 20240421/r6_0421_assingment_E/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace r6_0421_assingment_E {
-    internal class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
--- 20240421/r6_0421_assingment_G/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace r6_0421_assingment_G {
-    internal class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
