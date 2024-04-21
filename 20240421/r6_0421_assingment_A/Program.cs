15b2e38ca549eee4cc4b4c7326ca2985523469da
itotto <itotto0205@gmail.com>
2024-04-21 23:16:15 +0900

    不要なプロジェクトを削除

--- 20240421/r6_0421_assingment_A/Program.cs
+++ 20240421/r6_0421_assingment_A/Program.cs
@@ -1,7 +1,16 @@
-﻿namespace r6_0421_assingment_A {
+﻿using System.Collections.Generic;
+
+namespace r6_0421_assingment_A {
     internal class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+        /// <summary>
+        /// 01 Matrix Again
+        /// </summary>
+        /// <remarks>https://atcoder.jp/contests/arc176/tasks/arc176_a</remarks>
+        static void Main() {
+            var conditions1 = Console.ReadLine()?.Split(' ');
+            if (conditions1 == null) return;
+            var n = Convert.ToInt32(conditions1[0]);
+            var m = Convert.ToInt32(conditions1[1]);
         }
     }
 }
