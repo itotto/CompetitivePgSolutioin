beb1cb82da8c83e1eefc39890b6494178cf6c8d1
itotto <itotto0205@gmail.com>
2024-04-30 01:05:06 +0900

    A05_Three_Cards追加 A06_How_Many_Guests追加

--- books/Golden_rules_of_competitive_programming/A05_Three_Cards/Program.cs
+++ books/Golden_rules_of_competitive_programming/A05_Three_Cards/Program.cs
@@ -12,9 +12,15 @@ namespace A05_Three_Cards {
             var n = Convert.ToInt32(conditions[0]);
             var k = Convert.ToInt32(conditions[1]);
 
-            var redCardMax = 3000;
-
-            
+            var cnt = 0;
+            for (var i = 1; i <= n; i++) { // Red
+                for ( var j = 1; j <= n; j++) { // Blue
+                    var x = k - i - j;
+                    if (1 <= x && x <= n) cnt++;
+                    //Console.WriteLine($"{k} - {i} - {j} = {x}");
+                }
+            }
+            Console.WriteLine(cnt);
         }
     }
 }
---
+++ books/Golden_rules_of_competitive_programming/A06_How Many Guests/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace A06_How_Many_Guests {
+    internal class Program {
+        /// <summary>
+        /// A06 - How Many Guests
+        /// </summary>
+        /// <remarks>https://atcoder.jp/contests/tessoku-book/tasks/math_and_algorithm_ai</remarks>
+        static void Main() {
+            // 条件入力
+            var conditions1 = Console.ReadLine()?.Split(' ');
+            if (conditions1 == null) return;
+            var dayCountOfEvents = Convert.ToInt32(conditions1[0]);
+            var qCounts = Convert.ToInt32(conditions1[1]);
+
+            // 入場者数の合計を計算
+            var totalVisitors = new int[dayCountOfEvents];
+            var visitors = Console.ReadLine()?.Split(' ');
+            if (visitors == null) return;
+            totalVisitors[0] = Convert.ToInt32(visitors[0]);
+            for (int i = 1; i < visitors.Length; i++) {
+                totalVisitors[i] = totalVisitors[i - 1] + Convert.ToInt32(visitors[i]);
+            }
+
+            var answers = new List<int>(qCounts);
+            for(var i = 0; i < qCounts; i++) {
+                var conditions2 = Console.ReadLine()?.Split(' ');
+                if (conditions2 == null) return;
+                var fromDay = Convert.ToInt32(conditions2[0]);
+                var toDay = Convert.ToInt32(conditions2[1]);
+
+                if (fromDay == 1) {
+                    answers.Add(totalVisitors[toDay - 1]);
+                } else {
+                    answers.Add(totalVisitors[toDay - 1] - totalVisitors[fromDay - 2]);
+                }
+            }
+
+            // 結果の表示
+            foreach (var a in answers) Console.WriteLine(a);
+        }
+    }
+}
