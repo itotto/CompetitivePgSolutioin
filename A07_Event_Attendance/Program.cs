41b9fdf7abb3abfab2548a66e136439686495a94
itotto <itotto0205@gmail.com>
2024-05-04 20:55:15 +0900

    A07 - Event Attendance追加 A08 - Two Dimensional Sum追加(未了)

---
+++ A07_Event_Attendance/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace A07_Event_Attendance {
+    internal class Program {
+        /// <summary>
+        /// A07 - Event Attendance
+        /// </summary>
+        /// <remarks>https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_g</remarks>
+        static void Main() {
+            // イベント日数
+            var dayCount = Convert.ToInt32(Console.ReadLine());
+
+            // ユーザー数
+            var countOfUsers = Convert.ToInt32(Console.ReadLine());
+
+            // 初期化
+            var days = new int[dayCount];
+            for (var i = 0; i < dayCount; i++) days[i] = 0;
+
+            for (var i = 0; i < countOfUsers; i++) {
+                var conditions = Console.ReadLine()?.Split(' ');
+                if (conditions == null) return;
+                var fromDay = Convert.ToInt32(conditions[0]);
+                var toDay = Convert.ToInt32(conditions[1]);
+                days[fromDay - 1]++;
+                if (toDay < dayCount) days[toDay]--;
+            }
+
+            for (var i = 1; i < dayCount; i++) {
+                days[i] += days[i - 1];
+            }
+
+            foreach (var d in days) Console.WriteLine(d);
+        }
+    }
+}
---
+++ A08_Two_Dimensional_Sum/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace A08_Two_Dimensional_Sum {
+    internal class Program {
+        /// <summary>
+        /// A08 - Two Dimensional Sum
+        /// </summary>
+        /// <remarks>https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_h</remarks>
+        static void Main() {
+            var conditions = Console.ReadLine()?.Split(' ');
+            if (conditions == null) return;
+            var height = Convert.ToInt32(conditions[0]);
+            var width = Convert.ToInt32(conditions[1]);
+
+            // マス目の値をそのまま入力
+            var squares = new int[width, height];
+            for (var y = 0; y < height; y++) {
+                var columns = Console.ReadLine()?.Split(' ');
+                if (columns == null) return;
+                for (var x = 0; x < columns.Length; x++) {
+                    squares[y, x] = Convert.ToInt32(columns[x]);
+                }
+            }
+
+            // 横方向に足す
+            for (var y = 0; y < height; y++) {
+                for (var x = 1; x < width; x++) {
+                    squares[y, x] += squares[y, x - 1];
+                }
+            }
+
+            // 縦方向に足す
+            for (var x = 0; x < width; x++) {
+                for (var y = 1; y < height; y++) {
+                    squares[y, x] += squares[y - 1, x];
+                }
+            }
+
+            var result = new StringBuilder();
+
+            // 質問
+            var questionsCount = Convert.ToInt32(Console.ReadLine());
+            for (var i = 0; i < questionsCount; i++) {
+                var q = Console.ReadLine()?.Split(' ');
+                if (q == null) return;
+                var h1 = Convert.ToInt32(q[0]) - 1;
+                var w1 = Convert.ToInt32(q[1]) - 1;
+                var h2 = Convert.ToInt32(q[2]) - 1;
+                var w2 = Convert.ToInt32(q[3]) - 1;
+
+                var v1 = (w1 > 0 && h1 > 0) ? squares[h1 - 1, w1 - 1] : 0;
+                var v2 = w1 > 0 ? squares[h2, w1 - 1] : 0;
+                var v3 = h1 > 0 ? squares[h1 - 1, w2] : 0;
+                var v4 = squares[h2, w2];
+
+                result.AppendLine($"{v4 - v2 - v3 + v1}");
+            }
+
+            Console.Write(result.ToString());
+        }
+    }
+}
