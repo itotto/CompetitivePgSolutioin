d0b8db1421f70c618e3d91f6e1405285e74a65b6
itotto <itotto0205@gmail.com>
2024-05-13 17:25:50 +0900

    5/18分のプロジェクトを追加 5/17-27のHeuristic Contestを追加

---
+++ contests/20240518/r6_0518_assingment_B/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace r6_0518_assingment_B {
+    internal class Program {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <remarks></remarks>
+        static void Main() {
+        }
+    }
+}
---
+++ contests/20240518/r6_0518_assingment_C/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace r6_0518_assingment_C {
+    internal class Program {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <remarks></remarks>
+        static void Main() {
+        }
+    }
+}
