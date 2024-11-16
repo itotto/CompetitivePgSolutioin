8abbf649f2842a3d5f0912602ec14429cb61395e
itotto <itotto0205@gmail.com>
2024-11-17 02:52:14 +0900

    11月16日C変更

--- contests/2024/20241116/r6_1116_assingment_C/Program.cs
+++ contests/2024/20241116/r6_1116_assingment_C/Program.cs
@@ -17,24 +17,19 @@ namespace r6_1116_assingment_C {
             if (string.IsNullOrEmpty(line)) return;
 
             // 開始文字が 0 かどうか
-            var isZeroStart = line[0] == '0';
-            var lastChar = line[0];
+            var lastChar = ' ';
 
             // 塊
             var cluster = new StringBuilder();
-            cluster.Append(lastChar);
 
-            var oneClusters = new List<string>();
-            var zeroClusters = new List<string>();
-            for (var i = 1; i < n; i++) {
+            var clusters = new List<string>();
+            for (var i = 0; i < n; i++) {
                 var currentChar = line[i];
                 // 同じだったら塊に追加
                 if (currentChar == lastChar) {
                     cluster.Append(currentChar);
                 } else {
-                    if (lastChar == '0') zeroClusters.Add(cluster.ToString());
-                    else oneClusters.Add(cluster.ToString());
-
+                    clusters.Add(cluster.ToString());
                     cluster.Clear();
                     cluster.Append(currentChar);
                     lastChar = currentChar;
@@ -43,15 +38,18 @@ namespace r6_1116_assingment_C {
 
             // 最後の分が入ってなかったら追加
             if (cluster.Length > 0) {
-                if (line[line.Length - 1] == '0') zeroClusters.Add(cluster.ToString());
-                else oneClusters.Add(cluster.ToString());
+                clusters.Add(cluster.ToString());
             }
 
-            oneClusters[k - 2] += oneClusters[k - 1];
-            oneClusters.RemoveAt(k - 1);
+            var kIndex = line[0] == '0' ? 2 * k : 2 * k - 1;
 
+            clusters[kIndex - 2] += clusters[kIndex];
+            clusters.RemoveAt(kIndex);
 
-
+            for (var i = 1; i < clusters.Count; i++) {
+                Console.Write(clusters[i]);
+            }
+            Console.WriteLine();
         }
     }
 }
