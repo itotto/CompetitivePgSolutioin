6fd30573ca5481e8b1b5da38d9f62fd0d9ee36cb
itotto <itotto0205@gmail.com>
2026-04-12 02:34:56 +0900

    2026年2月28日と4月11日分を追加

---
+++ contests/2026/20260228/r8_0228_assingment_B/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Collections.Generic;
+
+namespace r8_0228_assingment_B {
+    internal class Program {
+        /// <summary>
+        /// B - mpp
+        /// </summary>
+        /// <remarks>https://atcoder.jp/contests/abc447/tasks/abc447_b</remarks>
+        static void Main() {
+            var s = Console.ReadLine();
+            if (string.IsNullOrEmpty(s)) return;
+
+            var countOfChars = new Dictionary<char, int>();
+            var maxCount = 0;
+
+            foreach (var c in s) {
+                if (countOfChars.TryGetValue(c, out var v)) {
+                    countOfChars[c] = ++v;
+                } else {
+                    v = 1;
+                    countOfChars.Add(c, v);
+                }
+                if (v > maxCount) maxCount = v;
+            }
+
+            var result = new StringBuilder();
+
+            foreach (var c in s) {
+                if (countOfChars[c] != maxCount) result.Append(c);
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ contests/2026/20260411/r8_0411_assingment_B/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace r8_0411_assingment_B {
+    internal class Program {
+        /// <summary>
+        /// B - Sensor Data Logging 
+        /// </summary>
+        /// <remarks>https://atcoder.jp/contests/abc453/tasks/abc453_b</remarks>
+        static void Main() {
+            var conditions1 = Console.ReadLine()?.Split(' ');
+            if (conditions1 == null) return;
+            var t = Convert.ToInt32(conditions1[0]);
+            var x = Convert.ToInt32(conditions1[1]);
+
+            var conditions2 = Console.ReadLine()?.Split(' ');
+            if (conditions2 == null) return;
+
+            var lastValue = Convert.ToInt32(conditions2[0]);
+            var result = new StringBuilder();
+            result.AppendLine($"0 {lastValue}");
+
+            for (var i = 1; i < conditions2.Length; i++) {
+                var currentValue = Convert.ToInt32(conditions2[i]);
+                var diff = Math.Abs(currentValue - lastValue);
+                if (diff < x) continue;
+
+                result.AppendLine($"{i} {currentValue}");
+                lastValue = currentValue;
+            }
+            if (result.Length == 0) Console.WriteLine();
+            else Console.Write(result.ToString());
+        }
+    }
+}
