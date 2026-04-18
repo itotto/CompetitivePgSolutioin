2b44fc39a7e9021aa6237d7e49f6b81799cf6566
itotto <itotto0205@gmail.com>
2026-04-19 03:04:08 +0900

    2026年4月18日分追加

---
+++ contests/2026/20260418/r8_0418_assingment_B/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace r8_0418_assingment_B {
+    internal class Program {
+        /// <summary>
+        /// B - Mapping
+        /// </summary>
+        /// <remarks>https://atcoder.jp/contests/abc454/tasks/abc454_b</remarks>
+        static void Main() {
+            var conditions1 = Console.ReadLine()?.Split(' ');
+            if (conditions1 == null) return;
+            var n = Convert.ToInt32(conditions1[0]);
+            var m = Convert.ToInt32(conditions1[1]);
+
+            var conditions2 = Console.ReadLine()?.Split(' ');
+            if (conditions2 == null) return;
+
+            var isDifferent = true;
+            var f2m = new Dictionary<int, int>();
+            for (var i = 0; i < conditions2.Length; i++) {
+                var f = Convert.ToInt32(conditions2[i]);
+                if (!f2m.ContainsKey(f)) f2m.Add(f, 1);
+                else {
+                    f2m[f]++;
+                    isDifferent = false;
+                }
+            }
+
+            Console.WriteLine(isDifferent ? "Yes" : "No");
+            Console.WriteLine(f2m.Count == m ? "Yes" : "No");
+        }
+    }
+}
